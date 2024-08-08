965088967d3f0a986d513b2155a83d303a9b770e
Admin <maayush235@gmail.com>
2024-08-08 19:00:11 +0530

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../components/my_textField.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //     logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+
+            // welcome back msg
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+
+
+            //   email textfield
+            const MyTextField()
+
+            //   pass textfield
+
+            //   login button
+
+            //   register now
+          ],
+        ),
+      ),
+    );
+  }
+}

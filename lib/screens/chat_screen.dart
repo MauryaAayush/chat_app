d6962f03d052be413d85dc756fbde6e0197bbd3f
Admin <maayush235@gmail.com>
2024-08-09 15:17:40 +0530

    3:00 PM

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/components/my_textField.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -43,6 +44,7 @@ class ChatScreen extends StatelessWidget {
           ),
 
           //   user input
+          _buildUserInput(),
         ],
       ),
     );
@@ -53,30 +55,46 @@ class ChatScreen extends StatelessWidget {
     return StreamBuilder(
       stream: _chatService.getMessage(receiverID, senderID),
       builder: (context, snapshot) {
-      //   error
-        if(snapshot.hasError){
+        //   error
+        if (snapshot.hasError) {
           return const Text('Error');
         }
 
-      //   loading
-        if(snapshot.connectionState == ConnectionState.waiting){
+        //   loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return const Text('Loading.....');
         }
 
-      // return list view
+        // return list view
         return ListView(
-          children: snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList()
-        );
+            children: snapshot.data!.docs
+                .map((doc) => _buildMessageItem(doc))
+                .toList());
       },
     );
   }
 
-  Widget _buildMessageItem(DocumentSnapshot doc){
+  Widget _buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
 
     return Text(data["message"]);
   }
 
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        //  textfield should take up most of the space
+        Expanded(
+          child: MyTextField(
+            hintText: "Type a message",
+            obscureText: false,
+            controller: _messageController,
+          ),
+        ),
 
+        IconButton(onPressed: sendMessage, icon: const Icon(Icons.arrow_upward))
 
+      ],
+    );
+  }
 }

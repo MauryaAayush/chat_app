43e94d10a52a33062e18f36623ae036cdca8f891
Admin <maayush235@gmail.com>
2024-08-09 13:40:28 +0530

    1:40 PM

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -70,4 +70,18 @@ class ChatService {
   }
 
 // get message
+  Stream<QuerySnapshot> getMessage(String userID, otherUserID) {
+// construct a chatroom ID for the two user
+
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("message")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }

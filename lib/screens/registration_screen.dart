51a7d1f052aedd9df08ec6ae7d6a0f7283f325af
Admin <maayush235@gmail.com>
2024-08-09 23:21:57 +0530

    3:00 PM

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -23,8 +23,7 @@ class RegisterScreen extends StatelessWidget {
     // if password match
     if (_txtcnfpass.text == _txtpass.text) {
       try {
-
-        _auth.signUpWithEmailPassword(_txtemail.text, _txtpass.text,_txtname.text,_txtmobile.text);
+        _auth.signUpWithEmailPassword(_txtemail.text, _txtpass.text);
       } catch (e) {
         showDialog(
           context: context,
@@ -50,116 +49,117 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //     logo
-            Icon(
-              Icons.message,
-              size: 60,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-
-            // welcome back msg
-            Text(
-              "Let's Create an account for you",
-              style: TextStyle(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //     logo
+              Icon(
+                Icons.message,
+                size: 60,
                 color: Theme.of(context).colorScheme.primary,
-                fontSize: 16,
               ),
-            ),
-
-            const SizedBox(
-              height: 25,
-            ),
-
-
-            // name textfield
-            MyTextField(
-              hintText: "Enter Your Name",
-              obscureText: false,
-              controller: _txtname,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-
-
-            // mobile number
-
-            MyTextField(
-              hintText: "Mobile Number",
-              obscureText: false,
-              controller: _txtmobile,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-
-
-            //   email textfield
-            MyTextField(
-              hintText: "Email",
-              obscureText: false,
-              controller: _txtemail,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            //   pass textfield
-            MyTextField(
-              hintText: "Password",
-              obscureText: true,
-              controller: _txtpass,
-            ),
-            // confirm password
-            const SizedBox(
-              height: 10,
-            ),
-            //   pass textfield
-            MyTextField(
-              hintText: "Confirm Password",
-              obscureText: true,
-              controller: _txtcnfpass,
-            ),
-            //   login button
-            const SizedBox(
-              height: 25,
-            ),
-
-            MyButton(
-              text: "Register",
-              onTap: () => register(context),
-            ),
-            //   register now
-            const SizedBox(
-              height: 25,
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Already have an account? ',
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.primary),
+              const SizedBox(
+                height: 50,
+              ),
+        
+              // welcome back msg
+              Text(
+                "Let's Create an account for you",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16,
                 ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Text(
-                    'Login now',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary),
+              ),
+        
+              const SizedBox(
+                height: 25,
+              ),
+        
+        
+              // name textfield
+              MyTextField(
+                hintText: "Enter Your Name",
+                obscureText: false,
+                controller: _txtname,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+        
+              // mobile number
+        
+              MyTextField(
+                hintText: "Mobile Number",
+                obscureText: false,
+                controller: _txtmobile,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+        
+        
+              //   email textfield
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: _txtemail,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //   pass textfield
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: _txtpass,
+              ),
+              // confirm password
+              const SizedBox(
+                height: 10,
+              ),
+              //   pass textfield
+              MyTextField(
+                hintText: "Confirm Password",
+                obscureText: true,
+                controller: _txtcnfpass,
+              ),
+              //   login button
+              const SizedBox(
+                height: 25,
+              ),
+        
+              MyButton(
+                text: "Register",
+                onTap: () => register(context),
+              ),
+              //   register now
+              const SizedBox(
+                height: 25,
+              ),
+        
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account? ',
+                    style:
+                        TextStyle(color: Theme.of(context).colorScheme.primary),
                   ),
-                ),
-              ],
-            )
-          ],
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      'Login now',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

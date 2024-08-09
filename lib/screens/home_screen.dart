7e6579f5f9eb3287b7550495e615845520d0e9cb
Admin <maayush235@gmail.com>
2024-08-09 08:55:29 +0530

    8:55 AM

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,20 +1,45 @@
-
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 import '../components/my_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
-
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Home'),
+        title: const Text('Home'),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: _buildUserList(),
+    );
+  }
+
+// build a list of users except for the current logged in user
+  Widget _buildUserList() {
+    return StreamBuilder(
+      stream: _chatService.getUserStream(),
+      builder: (context, snapshot) {
+        //   error
+          if(snapshot.hasData){
+            return const Text('Error');
+          }
+        //   loading....
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Text('Loading...');
+          }
+        //   return list view
+        return ListView(
+            children: snapshot.data!
+                .map<Widget>((userData) => _buildUserListItem)
+                .toList());
+      },
     );
   }
 }
--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatService{
-
+class ChatService {
 // get instance of firestore
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
@@ -21,14 +20,23 @@ class ChatService{
   ]
  */
 
+  Stream<List<Map<String, dynamic>>> getUserStream() {
+    return _firestore.collection("Users").snapshots().map(
+      (snapshot) {
+        return snapshot.docs.map(
+          (doc) {
+            //  go through each individual user
+            final user = doc.data();
 
-  Stream<List<Map<String,dynamic>>> getUserStream(){
-
+            //   return user
+            return user;
+          },
+        ).toList();
+      },
+    );
   }
 
 // send message
 
 // get message
-
-
 }

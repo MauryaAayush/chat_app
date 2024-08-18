d1b1ec34bf8d2a0317a9e89b5ee1a63f553ecfe9
Admin <maayush235@gmail.com>
2024-08-19 00:22:35 +0530

    Radhey radhey

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -5,7 +5,7 @@ import '../services/auth/auth_service.dart';
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
-  void logout (){
+  void logout() {
     final _auth = AuthService();
     _auth.signOut();
   }
@@ -17,54 +17,61 @@ class MyDrawer extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-
-
-         Column(
-           children: [
-             //   logo
-             DrawerHeader(
-               child: Center(
-                 child: Icon(
-                   Icons.message,
-                   color: Theme.of(context).colorScheme.primary,
-                   size: 40,
-                 ),
-               ),
-             ),
-             //   home list tile
-             Padding(
-               padding: const EdgeInsets.only(left: 25),
-               child: ListTile(
-                 title: const Text('H O M E'),
-                 leading: Icon(Icons.home,
-                   color: Theme.of(context).colorScheme.primary,),
-                 onTap: () {
-                   Navigator.pop(context);
-                 },
-               ),
-             ),
-             //    setting list tile
-             Padding(
-               padding: const EdgeInsets.only(left: 25),
-               child: ListTile(
-                 title: const Text('S E T T I N G S'),
-                 leading: Icon(Icons.settings,
-                   color: Theme.of(context).colorScheme.primary,),
-                 onTap: () {
-                   Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingScreen(),));
-                 },
-               ),
-
-             ),
-           ],
-         ),
+          Column(
+            children: [
+              //   logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40,
+                  ),
+                ),
+              ),
+              //   home list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: const Text('H O M E'),
+                  leading: Icon(
+                    Icons.home,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              //    setting list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  title: const Text('S E T T I N G S'),
+                  leading: Icon(
+                    Icons.settings,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SettingScreen(),
+                        ));
+                  },
+                ),
+              ),
+            ],
+          ),
           //   logoutlist tile
           Padding(
-            padding: const EdgeInsets.only(left: 25,bottom: 25),
+            padding: const EdgeInsets.only(left: 25, bottom: 25),
             child: ListTile(
               title: const Text('L O G O U T'),
-              leading: Icon(Icons.logout,
-                color: Theme.of(context).colorScheme.primary,),
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.primary,
+              ),
               onTap: logout,
             ),
           )
